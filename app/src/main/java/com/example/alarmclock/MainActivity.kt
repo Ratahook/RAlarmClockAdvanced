f0cd6c55aab08166ea39352281b5167ef7712598
rewqty <dmitriy.chelnokov.0000@gmail.com>
2022-12-17 18:31:32 +0500

    add alarm

--- app/src/main/java/com/example/alarmclock/MainActivity.kt
+++ app/src/main/java/com/example/alarmclock/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.example.alarmclock
 
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import android.os.Build
+import android.widget.Button
+import android.widget.TimePicker
+import android.widget.Toast
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +26,69 @@ class MainActivity : AppCompatActivity() {
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
             }
         })
+
+        val timePicker = findViewById<TimePicker>(R.id.picker)
+        timePicker.setIs24HourView(true)
+
+        val setAlarm = findViewById<Button>(R.id.setAlarm)
+
+        createNotificationChannel()
+
+        setAlarm.setOnClickListener {
+            val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+            val intent = Intent(this, AlarmReceiver::class.java)
+
+            val pendingIntent = PendingIntent.getBroadcast(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_IMMUTABLE
+            )
+
+            val alarmClockInfo = AlarmManager.AlarmClockInfo(
+                convertToAlarmTimeMillis(timePicker.hour, timePicker.minute),
+                pendingIntent)
+            alarmManager.setAlarmClock(alarmClockInfo, pendingIntent)
+
+            Toast.makeText(this, "Будильник установлен", Toast.LENGTH_SHORT).show()
+
+        }
+
+
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name : CharSequence = "wakeupReminderChannel"
+            val description = "Channel for Alarm Manager"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel("wakeup",name,importance)
+            channel.description = description
+            val notificationManager = getSystemService(
+                NotificationManager::class.java
+            )
+
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun convertToAlarmTimeMillis(hour: Int, minute: Int): Long {
+        val calendar = Calendar.getInstance()
+        val currentTimeMillis = calendar.timeInMillis
+
+        calendar.set(Calendar.HOUR_OF_DAY, hour)
+        calendar.set(Calendar.MINUTE, minute)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+
+        val proposedTimeMillis = calendar.timeInMillis
+
+        val alarmTimeMillis: Long = if (proposedTimeMillis > currentTimeMillis) {
+            proposedTimeMillis
+        } else {
+            proposedTimeMillis + 24 * 60 * 60 * 1000
+        }
+
+        return alarmTimeMillis
     }
 }

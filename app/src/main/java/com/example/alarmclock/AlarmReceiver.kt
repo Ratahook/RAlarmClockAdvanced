15e592c8c65761e3cf27190e94063f9c14cf8bdf
rewqty <dmitriy.chelnokov.0000@gmail.com>
2022-12-17 18:52:45 +0500

    Remove redundant code (again)

--- app/src/main/java/com/example/alarmclock/AlarmReceiver.kt
+++ app/src/main/java/com/example/alarmclock/AlarmReceiver.kt
@@ -1,6 +1,5 @@
 package com.example.alarmclock
 
-import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -10,16 +9,6 @@ import androidx.core.app.NotificationManagerCompat
 class AlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-
-        val pendingIntent = PendingIntent.getActivity(
-            context,
-            0,
-            Intent(context, MainActivity::class.java),
-            PendingIntent.FLAG_IMMUTABLE)
-
-
         val builder = NotificationCompat.Builder(context, "wakeup")
             .setSmallIcon(R.drawable.ic_launcher_background)
             .setContentTitle("Ratahook wake up")
@@ -27,7 +16,6 @@ class AlarmReceiver : BroadcastReceiver() {
             .setAutoCancel(true)
             .setDefaults(NotificationCompat.DEFAULT_ALL)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setContentIntent(pendingIntent)
 
         val notificationManager = NotificationManagerCompat.from(context)
         notificationManager.notify(123,builder.build())
